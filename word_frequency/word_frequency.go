cdfdb1f0906787616964d2c37c4d4466f5d10714
Leon Matthews <leon@digitaladvisor.nz>
2024-09-26 21:52:21 +1200

    Basic command-line handling

--- word_frequency/word_frequency.go
+++ word_frequency/word_frequency.go
@@ -1,9 +1,19 @@
 
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "log"
+    "os"
+)
 
 
 func main() {
-    fmt.Println("Word count")
+    log.SetFlags(0)
+    if len(os.Args) != 2 {
+        log.Fatal("usage: word_frequency PATH")
+    }
+
+    path := os.Args[1]
+    fmt.Println("Count words in %v", path)
 }
