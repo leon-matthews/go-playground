7b3bd0e6c1ec95dd15f819ce82f66e81b3fcb61e
Leon Matthews <leon@digitaladvisor.nz>
2024-10-09 12:23:22 +1300

    Create test helper function and use subtests

--- learn_go_with_tests/hello/hello.go
+++ learn_go_with_tests/hello/hello.go
@@ -2,8 +2,13 @@ package main
 
 import "fmt"
 
+const englishGreeting = "Hello, "
+
 func Hello(name string) string {
-	return "Hello, " + name
+	if name == "" {
+		name = "world"
+	}
+	return englishGreeting + name
 }
 
 func main() {
--- learn_go_with_tests/hello/hello_test.go
+++ learn_go_with_tests/hello/hello_test.go
@@ -2,10 +2,23 @@ package main
 
 import "testing"
 
-func TestHello(t *testing.T) {
-	got := Hello("Leon")
-	want := "Hello, Leon"
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
+}
+
+func TestHello(t *testing.T) {
+	t.Run("passing name as a parameter", func(t *testing.T) {
+		got := Hello("Leon")
+		want := "Hello, Leon"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("using default if name empty", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world"
+		assertCorrectMessage(t, got, want)
+	})
 }
