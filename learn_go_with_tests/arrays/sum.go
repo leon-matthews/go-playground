5039719a0a04fbd2499e145876927cc6745d835d
Leon Matthews <leon@digitaladvisor.nz>
2024-10-09 14:00:16 +1300

    Use range over slices, append to same, and assign fuctions to variables

--- learn_go_with_tests/arrays/sum.go
+++ learn_go_with_tests/arrays/sum.go
@@ -1,9 +1,35 @@
 package main
 
+// Add the integers in the given slice
 func Sum(numbers []int) int {
 	sum := 0
 	for _, number := range numbers {
 		sum += number
 	}
 	return sum
+}
+
+// Create a sum for every given slice
+func SumAll(nested ...[]int) []int {
+	var sums []int
+	for _, numbers := range nested {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+
+// Create a sum of all but the first element in ever given slice
+func SumAllTails(nested ...[]int) []int {
+	var sums []int
+	for _, numbers := range nested {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
 }
--- learn_go_with_tests/arrays/sum_test.go
+++ learn_go_with_tests/arrays/sum_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("array of five numbers", func(t *testing.T) {
@@ -23,5 +26,35 @@ func TestSum(t *testing.T) {
 		if got != want {
 			t.Errorf("got %d want %d, given %v", got, want, numbers)
 		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+	t.Run("sum just the tails", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+		checkSums(t, got, want)
+	})
+
+	t.Run("handle empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{2, 3, 5, 7})
+		want := []int{0, 15}
+		checkSums(t, got, want)
 	})
 }
