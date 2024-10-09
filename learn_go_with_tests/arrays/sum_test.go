b66a584129d24cc6545612cb13f4433a4c43e6f4
Leon Matthews <leon@digitaladvisor.nz>
2024-10-09 13:37:15 +1300

    Use a slice instead, and use test coverage

--- learn_go_with_tests/arrays/sum_test.go
+++ learn_go_with_tests/arrays/sum_test.go
@@ -3,12 +3,25 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [...]int{1, 2, 3, 4, 5}
+	t.Run("array of five numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
 
-	got := Sum(numbers)
-	want := 15
+		got := Sum(numbers)
+		want := 15
 
-	if got != want {
-		t.Errorf("got %d but wanted %d, given %v", got, want, numbers)
-	}
+		if got != want {
+			t.Errorf("got %d but wanted %d, given %v", got, want, numbers)
+		}
+	})
+
+	t.Run("slice of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+		got := Sum(numbers)
+		want := 55
+
+		if got != want {
+			t.Errorf("got %d want %d, given %v", got, want, numbers)
+		}
+	})
 }
