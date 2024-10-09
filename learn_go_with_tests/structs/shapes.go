23df7b93aa13bb27d3f2826d6c243ea7c670ed8e
Leon Matthews <leon@digitaladvisor.nz>
2024-10-09 19:36:09 +1300

    Implement table-driven tests to check new Triangle type

--- learn_go_with_tests/structs/shapes.go
+++ learn_go_with_tests/structs/shapes.go
@@ -30,3 +30,12 @@ func (r Rectangle) Area() float64 {
 func (r Rectangle) Perimeter() float64 {
 	return 2 * (r.Width + r.Height)
 }
+
+type Triangle struct {
+	Base float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) / 2.0
+}
--- learn_go_with_tests/structs/shapes_test.go
+++ learn_go_with_tests/structs/shapes_test.go
@@ -3,7 +3,7 @@ package structs
 import "testing"
 
 func TestArea(t *testing.T) {
-	// Check output of a shape's Area() method
+	// Helper to check output of any `Shape`s Area() method
 	checkArea := func(t testing.TB, shape Shape, want float64) {
 		t.Helper()
 		got := shape.Area()
@@ -21,6 +21,25 @@ func TestArea(t *testing.T) {
 		rectangle := Rectangle{5.0, 10.0}
 		checkArea(t, rectangle, 50.0)
 	})
+}
+
+func TestAreaTableDriven(t *testing.T) {
+	// Anonymous struct, slice of structs with two fields
+	areaTests := []struct {
+		shape Shape
+		want float64
+	} {
+		{shape: Circle{Radius: 10}, want: 314.1592653589793},
+		{shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
+		{shape: Triangle{Base: 12, Height: 6}, want: 36.0},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("%#v got %g want %g", tt.shape, got, tt.want)
+		}
+	}
 }
 
 func TestPerimeter(t *testing.T) {
