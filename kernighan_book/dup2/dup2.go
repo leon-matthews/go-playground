f690896d20555dc9fe74c416131f186091b79ad4
Leon Matthews <leon@digitaladvisor.nz>
2024-10-17 12:24:27 +1300

    Count lines by streaming line, by line.

--- kernighan_book/dup2/dup2.go
+++ kernighan_book/dup2/dup2.go
@@ -1,8 +1,42 @@
 // Print lines from stdin or given text files
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+}
 
 func main() {
-	fmt.Println("Dup Two Todo")
+	counts := make(map[string]int)
+	files := os.Args[1:]
+
+	// Perform count
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			countLines(f, counts)
+			f.Close()
+		}
+	}
+
+	// Print results
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
 }
