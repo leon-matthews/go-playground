df8b70e3ebe1339f9133a2ce1fcec0254c9499d4
Leon Matthews <leon@digitaladvisor.nz>
2024-10-18 16:06:34 +1300

    Another HTTP server, this time with a mutex

---
+++ kernighan_book/server2/server2.go
@@ -0,0 +1,33 @@
+// Add a counter to our echo server
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+)
+
+var mu sync.Mutex
+var count int
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/count", counter)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+// Display count
+func counter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	fmt.Fprintf(w, "Count %d\n", count)
+	mu.Unlock()
+}
+
+// Increment count
+func handler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	count++
+	mu.Unlock()
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
