b101f8214afdc39a711e1158bc3b6c14d0a6325d
Leon Matthews <leon@digitaladvisor.nz>
2024-10-22 14:39:51 +1300

    Finish concurrency example

--- learn_go_with_tests/concurrency/concurrency.go
+++ learn_go_with_tests/concurrency/concurrency.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	fmt.Println("concurrency")
@@ -8,11 +10,27 @@ func main() {
 
 type WebsiteChecker func(string) bool
 
+type result struct {
+	string
+	bool
+}
+
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	ch := make(chan result)
 
+	// Start one goroutine for each URL
+	// Each one sending a `result` back through `ch`
 	for _, url := range urls {
-		results[url] = wc(url)
+		go func(u string) {
+			ch <- result{u, wc(u)}
+		}(url)
+	}
+
+	// Receive results from `ch`
+	for range len(urls) {
+		r := <- ch
+		results[r.string] = r.bool
 	}
 
 	return results
