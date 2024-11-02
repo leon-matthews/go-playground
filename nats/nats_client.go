5724fd8430e5b166cedbdf85783b1215adba9a87
Leon Matthews <leon@digitaladvisor.nz>
2024-11-03 12:41:41 +1300

    Simple synchronous NATS client example

---
+++ nats/nats_client.go
@@ -0,0 +1,45 @@
+// Basic NATS usage examples.
+// Requires that a NATS server is running on the local system, eg.
+//
+//  $ sudo apt install nats-server
+//
+// For fun, also run the nats-cli command on a bunch of terminals:
+//
+//  $ nats subscribe foo
+
+package main
+
+import (
+	"fmt"
+	"github.com/nats-io/nats.go"
+	"log"
+	"time"
+)
+
+func main() {
+	fmt.Println("NATS client")
+	const url = nats.DefaultURL
+	const timeout = 1 * time.Second
+
+	// Connect to server on localhost
+	nc, err := nats.Connect(url)
+	if err != nil {
+		log.Fatalf("could not connect to default NATS server: %v", err)
+	}
+
+	// Create Synchronous Subscriber
+	sub, err := nc.SubscribeSync("foo")
+
+	// Publish message
+	err = nc.Publish("foo", []byte("Hello World"))
+	if err != nil {
+		log.Fatal("could not publish message: %v", err)
+	}
+
+	// Block until message received
+	msg, err := sub.NextMsg(timeout)
+	if err != nil {
+		log.Fatalf("could not receive message: %v", err)
+	}
+	fmt.Println(string(msg.Data))
+}
