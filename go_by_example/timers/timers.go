2a10fd4937c9e2f4bca97497c48d928e03cea8bf
Leon Matthews <leon.matthews@dbvisit.com>
2024-11-14 17:31:42 +1300

    Play around with timer example

--- go_by_example/timers/timers.go
+++ go_by_example/timers/timers.go
@@ -7,14 +7,25 @@ import (
 
 func main() {
 	timerExample()
+	waitForWholeSecond()
 	tickerExample()
 }
 
-// When the Timer expires, the current time will be sent on C
+// Set timer which expires when the current time has a whole number of seconds
+func waitForWholeSecond() {
+	now := time.Now()
+	ns := int64(1e9 - now.Nanosecond())
+	timer := time.NewTimer(time.Duration(ns))
+	fired := <-timer.C
+	fmt.Printf("Waited for %dns and fired at %v\n", ns, fired)
+}
+
+// When the Timer expires, the current time will be sent on channel 'C'
 func timerExample() {
 	// Can be used like [time.Sleep]
 	timer1 := time.NewTimer(500 * time.Millisecond)
 	<-timer1.C // blocks on the timer's channel C until it sends a value
+	fmt.Println("timer1 fired")
 
 	// Can also be cancelled before firing
 	timer2 := time.NewTimer(1 * time.Second)
@@ -24,8 +35,8 @@ func timerExample() {
 		fmt.Println("timer2 fired")
 	}()
 
-	stop2 := timer2.Stop()
-	if stop2 {
+	was_stopped := timer2.Stop()
+	if was_stopped {
 		fmt.Println("timer2 stopped")
 	}
 
@@ -34,7 +45,7 @@ func timerExample() {
 
 // Will send the current time on the channel after each tick
 func tickerExample() {
-	ticker := time.NewTicker(500 * time.Millisecond)
+	ticker := time.NewTicker(100 * time.Millisecond)
 	done := make(chan bool)
 
 	go func() {
