5dc07084feb5be872600b1c1db9dd13af64e0bbe
Leon Matthews <leon.matthews@dbvisit.com>
2024-11-28 16:59:54 +1300

    Start integration tests

---
+++ learn_go_with_tests/poker/server_integration_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestRecordAndRetrieveWins(t *testing.T) {
+	store := InMemoryStorage{}
+	server := PlayerServer{&store}
+	player := "Leon"
+
+	// On a winning streak!
+	server.ServeHTTP(httptest.NewRecorder(), postWinRequest(player))
+	server.ServeHTTP(httptest.NewRecorder(), postWinRequest(player))
+	server.ServeHTTP(httptest.NewRecorder(), postWinRequest(player))
+
+	// Check score
+	response := httptest.NewRecorder()
+	server.ServeHTTP(response, getScoreRequest(player))
+	assert.Equal(t, http.StatusOK, response.Code)
+	assert.Equal(t, "3", response.Body.String())
+}
