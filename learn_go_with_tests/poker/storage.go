f6f31627e10e3369eec4aaf4da55f0e544e7147e
Leon Matthews <leon.matthews@dbvisit.com>
2024-11-29 10:48:41 +1300

    Document storage interface

--- learn_go_with_tests/poker/storage.go
+++ learn_go_with_tests/poker/storage.go
@@ -1,23 +1,30 @@
 package main
 
+// PlayerStorage keeps track of players and their scores
 type PlayerStorage interface {
+	// GetPlayerScore returns number of wins by player with given name
+	// Returns zero if player not found, or has no wins.
 	GetPlayerScore(name string) int
+
+	// RecordWin saves a win for the given player
 	RecordWin(name string)
+
+	// Reset sets the scores to the given wins map. Use nil to zero-out wins.
 	Reset(wins map[string]int)
 }
 
+// NewInMemoryStorage builds a minimal storage for testing purposes
 func NewInMemoryStorage() *InMemoryStorage {
 	return &InMemoryStorage{
 		wins: make(map[string]int),
 	}
 }
 
+// InMemoryStorage is as simple interface implementation for testing
 type InMemoryStorage struct {
 	wins map[string]int
 }
 
-// GetPlayerScore returns number of wins by player with given name
-// Returns zero if player not found, or has no wins.
 func (s *InMemoryStorage) GetPlayerScore(name string) int {
 	score := s.wins[name]
 	return score
