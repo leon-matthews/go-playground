d624a2f5ba1cd92b87c786f0370171681576d9fa
Leon Matthews <leon@digitaladvisor.nz>
2024-12-08 09:53:51 +1300

    Minimal pflag & viper example

---
+++ command_line/viper/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+)
+
+func main () {
+	// Pflag replaces for Go's flag package, implementing GNU-style --flags.
+	pflag.StringP("name", "n", "Leon", "Name parameter")
+	pflag.StringP("password", "p", "hardToGuess", "Password")
+	pflag.Parse()
+
+	// Viper's handles configuration from env, files, and/or command-line, etc.
+	viper.BindPFlags(pflag.CommandLine)
+	fmt.Println(viper.GetString("password"))
+}
+
+func aliasNormalise(f *pflag.FlagSet, n string) pflag.NormalizedName {
+	switch n {
+	case "pass":
+		n = "password"
+	case "ps":
+		n  = "password"
+	}
+	return pflag.NormalizedName(n)
+}
