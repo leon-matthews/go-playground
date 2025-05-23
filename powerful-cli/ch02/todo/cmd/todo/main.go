69d6f66e16055a705943dcbd86a8c2aaf367676d
Leon Matthews <leon@lost.co.nz>
2025-05-24 10:57:41 +1200

    Use flags package instead of parsing manually

--- powerful-cli/ch02/todo/cmd/todo/main.go
+++ powerful-cli/ch02/todo/cmd/todo/main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"os"
-	"strings"
 
 	"powerful-cli/ch02/todo"
 )
@@ -11,29 +12,49 @@ import (
 const todoPath = "todo.json"
 
 func main() {
+	// Parse command-line flags
+	taskFlag := flag.String("task", "", "Add task to TODO list")
+	listFlag := flag.Bool("list", false, "List all tasks")
+	completeFlag := flag.Int("complete", 0, "Item to be completed")
+	flag.Parse()
+
 	// Attempt to load list
 	l := &todo.List{}
 	if err := l.Load(todoPath); err != nil {
-		fmt.Println(os.Stderr, err)
+		log.Fatalf("Loading list: %s", err)
 		os.Exit(1)
 	}
 
 	// Check args for action
 	switch {
 
-	// List current todo items
-	case len(os.Args) == 1:
+	case *listFlag:
+		// List current TODO items
 		for _, item := range *l {
-			fmt.Printf("\u2713 %v\n", item.Task)
+			if item.Done {
+				fmt.Printf("\u2713 %v\n", item.Task)
+			} else {
+				fmt.Printf("  %v\n", item.Task)
+			}
 		}
 
-	// Treat multiple arguments as a single string
-	default:
-		task := strings.Join(os.Args[1:], " ")
-		l.Add(task)
-		if err := l.Save(todoPath); err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(1)
+	case *completeFlag > 0:
+		// Mark task as done
+		if err := l.Complete(*completeFlag); err != nil {
+			log.Fatal(err)
 		}
+		if err := l.Save(todoPath); err != nil {
+			log.Fatal(err)
+		}
+
+	case *taskFlag != "":
+		// Add task to TODO list
+		l.Add(*taskFlag)
+		if err := l.Save(todoPath); err != nil {
+			log.Fatal(err)
+		}
+
+	default:
+		log.Fatal("Invalid option")
 	}
 }
