bd96c2f04898acd6111c65987c52ea5f728f3425
Leon Matthews <leon@lost.co.nz>
2025-06-07 11:50:21 +1200

    Finish basic rendering functionality

--- powerful-cli/ch03/mdp/main.go
+++ powerful-cli/ch03/mdp/main.go
@@ -3,24 +3,19 @@ package main
 
 import (
 	"bytes"
+	_ "embed"
 	"flag"
 	"fmt"
+	"text/template"
 	"os"
 	"path/filepath"
+
+	"github.com/microcosm-cc/bluemonday"
+	"github.com/russross/blackfriday/v2"
 )
 
-const (
-	header = `<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Markdown Preview Tool</title>
-</head>
-<body>`
-	footer = `
-</body>
-</html>`
-)
+//go:embed "template.html"
+var htmlTemplate string
 
 func main() {
 	filename := flag.String("file", "", "Markdown file to preview")
@@ -44,26 +39,35 @@ func run(filename string) error {
 	if err != nil {
 		return err
 	}
-
-	html, err := parseContent(contents)
+	body := parseContent(contents)
+	html, err := buildHTML(body)
 	if err != nil {
 		return err
 	}
-
 	outname := fmt.Sprintf("%s.html", filepath.Base(filename))
-	return saveHTML(outname, html)
+	err = os.WriteFile(outname, html, 0666)
+	return err
+
 }
 
-func parseContent(contents []byte) (string, error) {
-	var buffer bytes.Buffer
-	buffer.WriteString(header)
-	buffer.Write(contents)
-	buffer.WriteString(footer)
-	return buffer.String(), nil
+// parseContent converts Markdown file into sanitized HTML
+func parseContent(input []byte) []byte {
+	// Convert to HTML using default options
+	unsafe := blackfriday.Run(input)
+
+	// Sanitize HTML output using 'User Generated Content' policy
+	body := bluemonday.UGCPolicy().SanitizeBytes(unsafe)
+	return body
 }
 
-func saveHTML(filename string, html string) error {
-	fmt.Printf("=== %s ===", filename)
-	fmt.Println(html)
-	return nil
+// buildHTML embeds given body output using HTML template
+func buildHTML(body []byte) ([]byte, error) {
+	// Use text/template here, as we don't need to escape our generated HTML
+	t := template.Must(template.New("name").Parse(htmlTemplate))
+	var html bytes.Buffer
+	err := t.Execute(&html, string(body))
+	if err != nil {
+		return []byte{}, err
+	}
+	return html.Bytes(), nil
 }
