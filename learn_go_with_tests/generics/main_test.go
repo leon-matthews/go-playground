22a16d8801a223596c5e99ce49b53f72ec2980cc
Leon Matthews <leon.matthews@dbvisit.com>
2025-06-10 09:25:49 +1200

    Implement generic stack using comparable type constraint

---
+++ learn_go_with_tests/generics/main_test.go
@@ -0,0 +1,43 @@
+package main
+
+import "testing"
+
+func AssertEqual[T comparable](t *testing.T, got T, want T) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
+
+func AssertNotEqual[T comparable](t *testing.T, got, want T) {
+	t.Helper()
+	if got == want {
+		t.Errorf("didn't want %v", got)
+	}
+}
+
+func AssertTrue(t *testing.T, got bool) {
+	t.Helper()
+	if !got {
+		t.Errorf("got %v, want true", got)
+	}
+}
+
+func AssertFalse(t *testing.T, got bool) {
+	t.Helper()
+	if got {
+		t.Errorf("got %v, want false", got)
+	}
+}
+
+func TestAssertFunctions(t *testing.T) {
+    t.Run("asserting on integers", func(t *testing.T) {
+    	AssertEqual(t, 1, 1)
+	    AssertNotEqual(t, 1, 2)
+    })
+
+    t.Run("asserting on strings", func(t *testing.T) {
+    	AssertEqual(t, "Hello", "Hello")
+	    AssertNotEqual(t, "Hello", "world")
+    })
+}
