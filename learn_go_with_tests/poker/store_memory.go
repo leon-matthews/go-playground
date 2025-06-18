97a6cf004d7cb78660db7e7048a59e4255b8a3e4
Leon Matthews <leon.matthews@dbvisit.com>
2025-06-19 11:52:58 +1200

    Protect reads and writes using mutex

--- learn_go_with_tests/poker/store_memory.go
+++ learn_go_with_tests/poker/store_memory.go
@@ -1,18 +1,28 @@
 package main
 
+import "sync"
+
 // PlayerStoreMemory is a cheap in-memory implementation of PlayerStore
 type PlayerStoreMemory struct {
 	scores map[string]int
+	mu     sync.Mutex
 }
 
 func NewPlayerStoreMemory() *PlayerStoreMemory {
-	return &PlayerStoreMemory{map[string]int{}}
+	return &PlayerStoreMemory{
+		scores: map[string]int{},
+		mu:     sync.Mutex{},
+	}
 }
 
 func (s *PlayerStoreMemory) GetScore(name string) int {
+	s.mu.Lock()
+	defer s.mu.Unlock()
 	return s.scores[name]
 }
 
 func (s *PlayerStoreMemory) RecordWin(name string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
 	s.scores[name] += 1
 }
