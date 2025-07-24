4d269beadb5c985466fe8ef7b803acda8006851f
Leon Matthews <leon.matthews@dbvisit.com>
2025-07-24 12:28:21 +1200

    Temp example scanning through stdin

--- temp.go
+++ temp.go
@@ -1,15 +1,17 @@
-#!/usr/bin/env gorun
-
 package main
 
 import (
-    "fmt"
-    "slices"
+	"bufio"
+	"fmt"
+	"os"
 )
 
 func main() {
-    s := []string{"zero", "one", "two", "three", "four"}
-    for c := range slices.Chunk(s, 2) {
-        fmt.Print(c)
-    }
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text()) // Println will add back the final '\n'
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
 }
