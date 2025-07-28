5fa5d1dabd7a4819ba6b50dd8fe34545ba6b872d
Leon Matthews <leon.matthews@dbvisit.com>
2025-07-28 15:18:08 +1200

    Experiment with using sync.Pool, reorginise other examples from chapter

---
+++ concurrency-in-go-book/chapter-3/pool/pool.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"math/rand/v2"
+	"sync"
+)
+
+func main() {
+	var numCreated int
+
+	// The [New] member must return [any]
+	pool := &sync.Pool{
+		New: func() any {
+			numCreated++
+			mem := make([]byte, 1024)
+			return &mem
+		},
+	}
+
+	// Seed pool with four buffers
+	pool.Put(pool.New())
+	pool.Put(pool.New())
+	pool.Put(pool.New())
+	pool.Put(pool.New())
+
+	const numWorkers = 1024 * 1024
+	var wg sync.WaitGroup
+	wg.Add(numWorkers)
+	for range numWorkers {
+		go func() {
+			defer wg.Done()
+
+			// Get object - maybe from pool, maybe new
+			// We can may no assumptions about state of object
+			mem := pool.Get().(*[]byte)
+
+			// Do something with object
+			(*mem)[rand.N(1024)] = rand.N[byte](255)
+
+			// Put object back, otherwise there is no benefit!
+			defer pool.Put(mem)
+		}()
+
+	}
+	wg.Wait()
+
+    // Fetch and print a buffer to see its state. Probably messy by now!
+	mem := pool.Get().(*[]byte)
+	fmt.Println(*mem)
+
+	fmt.Println("Created", numCreated)
+}
---
+++ concurrency-in-go-book/chapter-3/rwmutex/once/once.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+    "fmt"
+    "sync"
+)
+
+func main() {
+    var count int
+    increment := func() {
+        count++
+    }
+    var once sync.Once
+
+    var wg sync.WaitGroup
+    wg.Add(100)
+    for range 100 {
+        go func() {
+            defer wg.Done()
+            once.Do(increment)
+            increment()
+        }()
+    }
+    wg.Wait()
+
+    fmt.Println("Count is", count)
+}
