b9972b60ea098a65c2dee58ce615b5b89840f15f
Leon Matthews <leon.matthews@dbvisit.com>
2025-08-01 12:49:23 +1200

    Primitive pipelines

---
+++ concurrency-in-go-book/chapter-4-concurrency-patterns/pipelines/pipelines.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Batch processing, no concurrency
+func main() {
+	nums := []int{1, 2, 3, 4, 5, 6,7 , 8 , 9, 10}
+	fmt.Println(nums)
+
+	nums = multiply(nums, 2)
+	fmt.Println(nums)
+
+	nums = add(nums, 2)
+	fmt.Println(nums)
+
+	nums = multiply(nums, 3)
+	fmt.Println(nums)
+}
+
+func add(values []int, additive int) []int {
+	output := make([]int, len(values))
+	for i, v := range values {
+		output[i] = v + additive
+	}
+	return output
+}
+
+func multiply(values []int, multiplier int) []int {
+	output := make([]int, len(values))
+	for i, v := range values {
+		output[i] = v * multiplier
+	}
+	return output
+}
