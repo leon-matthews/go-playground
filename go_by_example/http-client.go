3e39ff1338de66b9173e1e4c1d1b64a7fafbe50b
Leon Matthews <leon.matthews@dbvisit.com>
2025-08-05 11:23:28 +1200

    HTTP client & server examples

--- go_by_example/http-client.go
+++ go_by_example/http-client.go
@@ -7,15 +7,16 @@ import (
 )
 
 func main() {
-	resp, err := http.Get("https://gobyexample.com")
+	// Get is a wrapper around DefaultClient.Get; create new client to customise
+	resp, err := http.Get("https://gobyexample.com") // Returns *http.Response
 	if err != nil {
 		panic(err)
 	}
 	defer resp.Body.Close()
-	fmt.Println(resp.Status)
+	fmt.Println(resp.Request.URL, resp.Status)
 
 	scanner := bufio.NewScanner(resp.Body)
-	for i := 0; scanner.Scan() && i < 20; i++ {
+	for i := 0; scanner.Scan() && i < 10; i++ {
 		fmt.Println(scanner.Text())
 	}
 	if err := scanner.Err(); err != nil {
---
+++ go_by_example/http-server.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"maps"
+	"net/http"
+	"slices"
+)
+
+const port = "8080"
+
+func main() {
+	// Simple function must first be cast to a type that implements [Handler]...
+	helloHandler := http.HandlerFunc(hello)
+	http.Handle("/hello", helloHandler)
+
+	// ...which can be achieved via a helper
+	http.HandleFunc("/headers", headers)
+
+	fmt.Printf("Running server on port %s\n", port)
+	err := http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// Print hello directly to writer
+func hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "Hello, world!")
+}
+
+// headers prints request's headers out in alphabetical order
+func headers(w http.ResponseWriter, req *http.Request) {
+	keys := slices.Collect(maps.Keys(req.Header))
+	slices.Sort(keys)
+	for _, key := range keys {
+		value := req.Header.Get(key)
+		fmt.Fprintf(w, "%v: %v\n", key, value)
+	}
+}
