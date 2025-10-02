bffd4ccd0147a4e2d541ce8536dba2b744d6e675
Leon Matthews <leon.matthews@dbvisit.com>
2025-10-03 10:59:57 +1300

    Data race detection detecting

--- temp.go
+++ temp.go
@@ -2,21 +2,16 @@ package main
 
 import (
 	"fmt"
-	"math/rand/v2"
+	"sync"
 )
 
 func main() {
-	var seed = [32]byte{0x01}
-	source := rand.NewChaCha8(seed)
-
-	// Create 1KiB of random bytes
-	b := make([]byte, 1_024)
-	source.Read(b)
-	fmt.Println(b)
-
-	// Create the exact same bytes
-	source.Seed(seed)
-	b2 := make([]byte, 1_024)
-	source.Read(b2)
-	fmt.Println(b2)
+    // Data race! Run with:
+    // go run --race temp.go
+    var total int
+    var wg sync.WaitGroup
+    wg.Go(func() { total++ })
+    wg.Go(func() { total++ })
+    wg.Wait()
+    fmt.Println("total:", total)
 }
