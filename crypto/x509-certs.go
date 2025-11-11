ffec42dc548047e95a8762a1e96f5121c62a4e71
Leon Matthews <leon.matthews@dbvisit.com>
2025-11-12 10:54:47 +1300

    Create PEM encoded x509 ed25519 public key pair

---
+++ crypto/x509-certs.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"bytes"
+	"crypto/ed25519"
+	"crypto/rand"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"fmt"
+	"log"
+	"math/big"
+	"time"
+)
+
+func main() {
+	// Script based on published code from the Go stdlib:
+	// https://go.dev/src/crypto/tls/generate_cert.go
+
+	// Create self-signed Certificate Authority (CA)
+	ca := CreateCertificateAuthority()
+
+	// Create private key
+	_, caPrivKey, err := ed25519.GenerateKey(rand.Reader)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Combine to create self-signed certificate
+	caBytes, err := x509.CreateCertificate(rand.Reader, ca, ca, caPrivKey.Public(), caPrivKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// PEM encode certificate
+	caPEM := new(bytes.Buffer)
+	pem.Encode(caPEM, &pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: caBytes,
+	})
+	log.Println("PEM encode certificate key\n", caPEM)
+
+	// PEM encode private key
+	caPrivKeyPEM := new(bytes.Buffer)
+	privBytes, err := x509.MarshalPKCS8PrivateKey(caPrivKey)
+	if err != nil {
+		log.Fatalf("Unable to marshal private key: %v", err)
+	}
+	pem.Encode(caPrivKeyPEM, &pem.Block{Bytes: privBytes, Type: "PRIVATE KEY"})
+	log.Println("PEM encode private key\n", caPrivKeyPEM)
+}
+
+// CreateCertificateAuthority creates self-signed CA
+func CreateCertificateAuthority() *x509.Certificate {
+	ca := &x509.Certificate{
+		SerialNumber: CreateSerialNumber(),
+		Subject: pkix.Name{
+			Organization:  []string{"The Lost Continent of"},
+			Country:       []string{"NZ"},
+			Province:      []string{""},
+			Locality:      []string{"San Francisco"},
+			StreetAddress: []string{"Auckland"},
+			PostalCode:    []string{"90210"},
+		},
+		NotBefore:             time.Now(),
+		NotAfter:              time.Now().AddDate(10, 0, 0),
+		IsCA:                  true,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
+		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
+		BasicConstraintsValid: true,
+	}
+	log.Println("Create self-signed certificate", ca)
+	fmt.Printf("[%T]%+[1]v\n", CreateSerialNumber())
+	return ca
+}
+
+// CreateSerialNumber creates a cryptographically-secure 128-bit random number
+// Will panic if the crypto/rand.Reader
+func CreateSerialNumber() *big.Int {
+	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128) // 2^128
+	s, err := rand.Int(rand.Reader, serialNumberLimit)
+	if err != nil {
+		panic(fmt.Sprint("Random number generation failed: %v", err))
+	}
+	return s
+}
