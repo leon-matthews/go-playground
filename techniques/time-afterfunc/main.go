4333a8f79c44eddedd91c529f92594f34dd51e30
Leon Matthews <leon.matthews@dbvisit.com>
2026-02-04 09:09:08 +1300

    Start time.AfterFunc() example

--- techniques/time-afterfunc/main.go
+++ techniques/time-afterfunc/main.go
@@ -7,13 +7,55 @@ import (
 
 func main() {
 	leakyTickerExample()
+	goroutinelessAfterFuncExample()
 }
 
+// afterFuncExample() ----------------------------------------------------------
+func goroutinelessAfterFuncExample() {
+	log.Println("Starting goroutine-less example using time.AfterFunc()")
+	t := newAfterFuncExample(time.Second * 1)
+	t.loop()
+
+	time.Sleep(3 * time.Second)
+	log.Println("Send stop")
+	t.stop()
+}
+
+type afterFuncExample struct {
+	latency time.Duration
+	t       *time.Timer
+}
+
+func newAfterFuncExample(latency time.Duration) *afterFuncExample {
+	return &afterFuncExample{
+		latency: latency,
+		t:       nil,
+	}
+}
+
+func (t *afterFuncExample) action() {
+	log.Println("Action ticked")
+}
+
+func (t *afterFuncExample) loop() {
+	if t.t == nil {
+		t.t = time.AfterFunc(t.latency, t.action)
+	} else {
+		t.t.Reset(t.latency)
+	}
+}
+
+func (t *afterFuncExample) stop() {
+	t.t.Stop()
+}
+
+// tickerExample ---------------------------------------------------------------
 func leakyTickerExample() {
+	log.Println("Starting leaky ticker")
 	t := newTickerExample(time.Second * 1)
 	go t.loop()
 
-	// If stop is never called, the goroutine above will never exit
+	// If `t.stop()` is never called, the goroutine above will never exit
 	time.Sleep(3 * time.Second)
 	log.Println("Send stop")
 	t.stop()
