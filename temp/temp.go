d2862c0387bf61e2c2eb60e9eb2f7d2da58fe061
Leon Matthews <leon.matthews@dbvisit.com>
2026-02-25 09:17:55 +1300

    Close channel to signal end-of-data

--- temp/temp.go
+++ temp/temp.go
@@ -2,9 +2,29 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Temp!")
-}
+	str := "one,two,,four"
 
+	in := make(chan string)
+	go func() {
+		words := strings.Split(str, ",")
+		for _, word := range words {
+			in <- word
+		}
+		close(in)
+	}()
+
+	for {
+		word, ok := <-in
+		if !ok {
+			break
+		}
+
+		if word != "" {
+			fmt.Printf("%s ", word)
+		}
+	}
+}
