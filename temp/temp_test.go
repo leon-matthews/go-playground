d2563a4d7d2f8d58daec4662e8cbdec159f334c1
Leon Matthews <leon.matthews@dbvisit.com>
2026-02-25 15:58:25 +1300

    Benchmark iteration speed-up on strings.Split() too

--- temp/temp_test.go
+++ temp/temp_test.go
@@ -8,6 +8,26 @@ import (
 var loremIpsum = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed` +
 	`do eiusmod tempor incididunt ut labore et dolore magna aliqua. `
 
+func BenchmarkStringSplit(b *testing.B) {
+	for b.Loop() {
+		for _, w := range strings.Split(loremIpsum, " ") {
+			if w == "adipiscing" {
+				break
+			}
+		}
+	}
+}
+
+func BenchmarkStringSplitSeq(b *testing.B) {
+	for b.Loop() {
+		for w := range strings.SplitSeq(loremIpsum, " ") {
+			if w == "adipiscing" {
+				break
+			}
+		}
+	}
+}
+
 func BenchmarkStringFields(b *testing.B) {
 	for b.Loop() {
 		for _, w := range strings.Fields(loremIpsum) {
