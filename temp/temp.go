a0c18ccbd251e29d8be19f57b6c2cf7c1441dff0
Leon Matthews <leon.matthews@dbvisit.com>
2026-03-10 11:26:37 +1300

    Play around with jitter measurement

--- temp/temp.go
+++ temp/temp.go
@@ -2,24 +2,34 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"math/rand/v2"
+	"time"
 )
 
 func main() {
-	str := "one,two,,four"
-
-	in := make(chan string)
-	go func() {
-		words := strings.Split(str, ",")
-		for _, word := range words {
-			in <- word
-		}
-		close(in)
-	}()
-
-	for word := range in {
-		if word != "" {
-			fmt.Printf("%s ", word)
-		}
+	var average, largest, smallest time.Duration
+	for {
+		j := jitter()
+		largest = max(largest, j)
+		smallest = min(smallest, j)
+		average = emv(j, average, 0.1)
+		fmt.Printf("\ravg: %6v, min: %6v\tmax: %6v\t", average, smallest, largest)
 	}
+}
+
+// emv calculates a rough moving average for the given durations
+func emv(current, previous time.Duration, alpha float64) time.Duration {
+	output := alpha*float64(current) + (1.0-alpha)*float64(previous)
+	return time.Duration(output)
+}
+
+// jitter measures the difference between the requested and actual delay for a random timer duration
+func jitter() time.Duration {
+	expected := rand.N(100 * time.Millisecond)
+	timer := time.NewTimer(expected)
+	start := time.Now()
+	<-timer.C
+	actual := time.Since(start)
+	delta := actual - expected
+	return delta
 }
