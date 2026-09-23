7f64e4e59212e573624cf36f88b443b4b9854987
Leon Matthews <leon@lost.co.nz>
2026-09-23 23:39:34 +1200

    Start maps chapter

---
+++ learn-go-with-tests/maps/dictionary_test.go
@@ -0,0 +1,17 @@
+package main
+
+import "testing"
+
+func TestSearch(t *testing.T) {
+	dict := Dictionary{"test": "This is just a test"}
+	got := Search(dict, "test")
+	want := "This is just a test"
+	assertStrings(t, got, want)
+}
+
+func assertStrings(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
