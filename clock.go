1f4d23a25f19ce15e3c07554973d2e42c6c8da09
Leon Matthews <leon.matthews@dbvisit.com>
2026-09-24 09:41:06 +1200

    Create timed sleep clock tick playground

--- clock.go
+++ clock.go
@@ -9,10 +9,10 @@ func main() {
 	for {
 		// Print the time
 		now := time.Now()
-		fmt.Print(now.Format("15:04:05.000") + "\r")
+		fmt.Println(now.Format("15:04:05.000"))
 
-		// Sleep until exact time
-		next := now.Truncate(10 * time.Second).Add(10 * time.Second)
+		// Sleep until exact time. Refetch time to avoid time taken to print
+		next := time.Now().Truncate(10 * time.Second).Add(10 * time.Second)
 		time.Sleep(time.Until(next))
 	}
 }
